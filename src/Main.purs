8d41e663ede5ab39343d3772925de19823701e29
Robert Porter <robertp.dev@gmail.com>
2018-05-17 02:24:13 +0900

    Removed unnecessary reference to EXPRESS effect

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,9 @@ import Prelude
 import Api.Server (runServer)
 import Api.Types (ApiEffects)
 import Control.Monad.Eff (Eff)
-import Node.Express.Types (EXPRESS)
 
 
-main :: forall e. Eff (ApiEffects ( express :: EXPRESS | e )) Unit
+main :: forall e. Eff (ApiEffects e) Unit
 main = runServer port
   where
     port = 8080
